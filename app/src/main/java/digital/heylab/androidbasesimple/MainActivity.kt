58996adde7d944451aa14ea1d946b96721733b7c
Gabriel Medeiros <gabrielsme@gmail.com>
2021-06-02 19:32:01 -0300

    ViewBinding

--- app/src/main/java/digital/heylab/androidbasesimple/MainActivity.kt
+++ app/src/main/java/digital/heylab/androidbasesimple/MainActivity.kt
@@ -2,10 +2,20 @@ package digital.heylab.androidbasesimple
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import digital.heylab.androidbasesimple.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.texto.setOnClickListener {
+            Toast.makeText(this, "Eita porra", Toast.LENGTH_SHORT).show()
+        }
     }
 }
